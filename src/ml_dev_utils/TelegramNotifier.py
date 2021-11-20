608b4261f6afe96dff6bf78703131e671ce96ec7
guen-ltu <christian.gunther@ltu.se>
2021-11-20 10:55:46 +0100

    feature: add telegram notifier

---
+++ src/ml_dev_utils/TelegramNotifier.py
@@ -0,0 +1,32 @@
+import os
+import requests
+
+
+class TelegramNotifier:
+
+    def __init__(self):
+        self.bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+        self.chat_id = os.getenv('TELEGRAM_CHAT_ID')
+
+    def send(self, message):
+        if self.bot_token == None or self.chat_id == None:
+            return {
+                'ok': False,
+                'error_code': 404,
+                'description': '"No telegram settings set. Skipping notification"'
+            }
+
+        send_message_url = (f'https://api.telegram.org'
+                            f'/bot{self.bot_token}/sendMessage?'
+                            f'chat_id={self.chat_id}'
+                            f'&parse_mode=Markdown'
+                            f'&text={message}')
+        response = requests.get(send_message_url)
+
+        return response.json()
+
+
+if __name__ == "__main__":
+    telegram_notifier = TelegramNotifier()
+    response = telegram_notifier.send("Training finished with 99,9 accuracy!")
+    print(response)
